daec87a03933d603be6d63223aa618996b1b43c4
Omkar Modhave <omkarmodhave2@gmail.com>
2026-05-09 13:09:04 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
